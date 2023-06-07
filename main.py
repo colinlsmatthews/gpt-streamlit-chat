8e1f18631e65a142200ee8437ffa8da36e363900
colin matthews <matthews.colin@proton.me>
2023-06-07 09:30:18 -0400

    add token count for messages

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 import os
 import openai
+import tiktoken
 
 #declare global variables
 openai.organization = "org-VQ8Slx5WR2WUeXOCdbA68dqR"
@@ -22,20 +23,38 @@ def completion(_model, _prompt, _max_tokens, _temperature):
         max_tokens = _max_tokens,
         temperature = _temperature
     )
+def num_tokens_from_messages(messages, model="gpt-3.5-turbo-0301"):
+    """Returns the number of tokens used by a list of messages."""
+    try:
+        encoding = tiktoken.encoding_for_model(model)
+    except KeyError:
+        print("Warning: model not found. Using cl100k_base encoding.")
+        encoding = tiktoken.get_encoding("cl100k_base")
+    if model == "gpt-3.5-turbo":
+        print("Warning: gpt-3.5-turbo may change over time. Returning num tokens assuming gpt-3.5-turbo-0301.")
+        return num_tokens_from_messages(messages, model="gpt-3.5-turbo-0301")
+    elif model == "gpt-4":
+        print("Warning: gpt-4 may change over time. Returning num tokens assuming gpt-4-0314.")
+        return num_tokens_from_messages(messages, model="gpt-4-0314")
+    elif model == "gpt-3.5-turbo-0301":
+        tokens_per_message = 4  # every message follows <|start|>{role/name}\n{content}<|end|>\n
+        tokens_per_name = -1  # if there's a name, the role is omitted
+    elif model == "gpt-4-0314":
+        tokens_per_message = 3
+        tokens_per_name = 1
+    else:
+        raise NotImplementedError(f"""num_tokens_from_messages() is not implemented for model {model}. See https://github.com/openai/openai-python/blob/main/chatml.md for information on how messages are converted to tokens.""")
+    num_tokens = 0
+    for message in messages:
+        num_tokens += tokens_per_message
+        for key, value in message.items():
+            num_tokens += len(encoding.encode(value))
+            if key == "name":
+                num_tokens += tokens_per_name
+    num_tokens += 3  # every reply is primed with <|start|>assistant<|message|>
+    return num_tokens
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi(user)
-
-#print(completion("text-davinci-003", "Say this is a test", 7, 0))
-
-messages = [
-    {"role": "system",
-     "content": "You are a marketing coordinator for an architecture firm with the following profile: {}".format(
-         profile)}
-]
-def run(messages):
+def chat(messages):
     print("_____________________________________")
     user_input = input("Write a prompt (type \"exit\" to end conversation)\n_____________________________________\n\n")
     user_message = {"role": "user", "content": user_input}
@@ -56,11 +75,24 @@ def run(messages):
         content = response['choices'][0]['message']['content']
         gpt_message = {"role": f"{role}", "content": f"{content}" }
         messages.append(gpt_message)
+        num_tokens = num_tokens_from_messages(messages, model="gpt-3.5-turbo-0301")
+        print(f"Number of tokens used: {num_tokens}")
         #print(messages)
 
     except ValueError:
         print("Invalid input. Please enter a text prompt.")
 
-    run(messages)
+    chat(messages)
 
-run(messages)
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi(user)
+
+#print(completion("text-davinci-003", "Say this is a test", 7, 0))
+messages = [
+    {"role": "system",
+     "content": "You are a marketing coordinator for an architecture firm with the following profile: {}".format(
+         profile)}
+]
+
+chat(messages)
