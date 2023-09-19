38d8645f5c3aece7775c8f3e6546d1a1dbab0025
colin matthews <matthews.colin@proton.me>
2023-09-19 10:08:18 -0400

    delete deprecated SQL workflow prior to Firestore dev

--- pages/1_Profile Manager.py
+++ pages/1_Profile Manager.py
@@ -3,46 +3,36 @@ import streamlit as st
 from sqlalchemy import text
 import EAGPT_lib as eagpt
 
-profile_schema = {
-    "name": "TEXT PRIMARY KEY",
-    "content": "TEXT",
-    "description": "TEXT"
-}
+st.title("Profile Manager")
+st.markdown(
+    '''
+    Utility for managing, editing, and sharing system profiles.
+    
+    '''
+)
 
+# Deprecated SQL workflow
 
-eagpt.update_db(db_filename="profiles.db", clear=True, schema=profile_schema)
+# profile_schema = {
+#     "name": "TEXT PRIMARY KEY",
+#     "content": "TEXT",
+#     "description": "TEXT"
+# }
 
-# Create the SQL connection to pets_db as specified in your secrets file.
-# conn = st.experimental_connection('pets_db', type='sql')
+# eagpt.update_db(db_filename="profiles.db", clear=True, schema=profile_schema)
 
-# # Insert some data with conn.session.
-# with conn.session as s:
-#     t1 = text('CREATE TABLE IF NOT EXISTS pet_owners (person TEXT, pet TEXT);')
-#     s.execute(t1)
-#     t2 = text('DELETE FROM pet_owners;')
-#     s.execute(t2)
-#     pet_owners = {'jerry': 'fish', 'barbara': 'cat', 'alex': 'puppy'}
-#     for k in pet_owners:
-#         t3 = text('INSERT INTO pet_owners (person, pet) VALUES (:owner, :pet);')
-#         s.execute(t3, params=dict(owner=k, pet=pet_owners[k]))
-#     s.commit()
+# if st.button("Clear DB"):
+#     eagpt.clear_db(db_filename="profiles.db")
 
-# # Query and display the data you inserted
-# pet_owners = conn.query('select * from pet_owners')
-# st.dataframe(pet_owners)
+# input = st.text_input("Enter profile name")
 
-if st.button("Clear DB"):
-    eagpt.clear_db(db_filename="profiles.db")
+# if st.button("Add profile"):
+#     eagpt.add_profile_to_db(
+#         name=input,
+#         description=input,
+#         content=input
+#     )
 
-input = st.text_input("Enter profile name")
-
-if st.button("Add profile"):
-    eagpt.add_profile_to_db(
-        name=input,
-        description=input,
-        content=input
-    )
-
-conn2 = st.experimental_connection('profiles_db', type='sql')
-profiles = conn2.query('select * from profiles')
-st.dataframe(profiles)
+# conn2 = st.experimental_connection('profiles_db', type='sql')
+# profiles = conn2.query('select * from profiles')
+# st.dataframe(profiles)
