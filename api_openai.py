88e6dcefa8f6e4c0158aaea60d9a0774a2810ed2
colin matthews <matthews.colin@proton.me>
2023-09-20 11:55:02 -0400

    add api library files

---
+++ api_openai.py
@@ -0,0 +1,66 @@
+import openai
+import tiktoken
+
+
+def get_model_list(sort=False):
+    model_id_list = []
+    for openai_model_dict in openai.Model.list()["data"]:
+        model_id_list.append(openai_model_dict["id"])
+    gpt_models = []
+    for model in model_id_list:
+        if model[0:3].lower() == "gpt":
+            gpt_models.append(model)
+        else:
+            pass
+    if sort == True:
+        gpt_models.sort()
+        return gpt_models
+    elif sort == False:
+        return gpt_models
+
+
+def num_tokens_from_messages(messages, model="gpt-3.5-turbo-0613"):
+    """Return the number of tokens used by a list of messages."""
+    try:
+        encoding = tiktoken.encoding_for_model(model)
+    except KeyError:
+        print("Warning: model not found. Using cl100k_base encoding.")
+        encoding = tiktoken.get_encoding("cl100k_base")
+    if model in {
+        "gpt-3.5-turbo-0613",
+        "gpt-3.5-turbo-16k-0613",
+        "gpt-4-0314",
+        "gpt-4-32k-0314",
+        "gpt-4-0613",
+        "gpt-4-32k-0613",
+    }:
+        tokens_per_message = 3
+        tokens_per_name = 1
+    elif model == "gpt-3.5-turbo-0301":
+        # every message follows <|start|>{role/name}\n{content}<|end|>\n
+        tokens_per_message = 4
+        tokens_per_name = -1  # if there's a name, the role is omitted
+    elif "gpt-3.5-turbo" in model:
+        print("Warning: gpt-3.5-turbo may update over time. Returning num tokens assuming gpt-3.5-turbo-0613.")
+        return num_tokens_from_messages(messages, model="gpt-3.5-turbo-0613")
+    elif "gpt-4" in model:
+        print(
+            "Warning: gpt-4 may update over time. Returning num tokens assuming gpt-4-0613.")
+        return num_tokens_from_messages(messages, model="gpt-4-0613")
+    else:
+        raise NotImplementedError(
+            f"""num_tokens_from_messages() is not implemented for model {model}. See https://github.com/openai/openai-python/blob/main/chatml.md for information on how messages are converted to tokens."""
+        )
+    num_tokens = 0
+    for message in messages:
+        num_tokens += tokens_per_message
+        for key, value in message.items():
+            num_tokens += len(encoding.encode(value))
+            if key == "name":
+                num_tokens += tokens_per_name
+    num_tokens += 3  # every reply is primed with <|start|>assistant<|message|>
+    return num_tokens
+
+
+if __name__ == "__main__":
+    print("This is a library.")
